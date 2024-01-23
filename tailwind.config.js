eaaba5c9f2b79589ba492c278d2f1fb1783c00d0
AkinoJoey <gakiya213@gmail.com>
2024-01-23 17:53:51 +0900

    daisyUIを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/Views/components/*.{html,js}",
-    "./node_modules/flowbite/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("flowbite/plugin")],
+	content: ["./src/views/components/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("flowbite/plugin"), require("daisyui")],
 };
