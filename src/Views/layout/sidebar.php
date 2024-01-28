7c1b791e6b5a5b4b575de476773f609b90985b69
AkinoJoey <gakiya213@gmail.com>
2024-01-28 15:26:56 +0900

    cssのlinkを削除

--- src/Views/layout/sidebar.php
+++ src/Views/layout/sidebar.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="./css/output.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -113,4 +112,4 @@
     </div>
 
 
-    <script src="/node_modules/flowbite/dist/flowbite.min.css"></script>
+    <script src="./bundle.js"></script>
