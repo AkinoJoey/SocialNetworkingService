96fad4ab526cb21a26a9b4538f50aa7738961bad
AkinoJoey <gakiya213@gmail.com>
2024-01-28 17:55:59 +0900

    各ページのgetを作成

--- src/routing/routes.php
+++ src/routing/routes.php
@@ -1,9 +1,22 @@
 <?php
 
+use src\response\HTTPRenderer;
 use src\response\render\HTMLRenderer;
 
 return [
-    'signup' =>function(): HTMLRenderer{
+    '' => function (): HTTPRenderer {
+        return new HTMLRenderer('page/top');
+    },
+    'guest' =>function () : HTTPRenderer{
+        return new HTMLRenderer('page/guest');
+    },
+    'signup' =>function(): HTTPRenderer{
         return new HTMLRenderer('page/signup');
-    }
+    },
+    'login' =>function () : HTTPRenderer{
+        return new HTMLRenderer('page/login');
+    },
+    'profile' => function (): HTTPRenderer {
+        return new HTMLRenderer('page/profile');
+    },
 ];
