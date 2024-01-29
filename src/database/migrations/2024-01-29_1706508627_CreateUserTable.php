2da88351d9bb4e1a6fb9877588984522c5cb343c
AkinoJoey <gakiya213@gmail.com>
2024-01-29 17:14:38 +0900

    usersテーブル作成のマイグレーションを作成

---
+++ src/database/migrations/2024-01-29_1706508627_CreateUserTable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace src\database\migrations;
+
+use src\database\SchemaMigration;
+
+class CreateUserTable implements SchemaMigration
+{
+    public function up(): array
+    {
+        return [
+            "CREATE TABLE users (
+                id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                account_name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                username VARCHAR(255) UNIQUE,
+                email_verified VARCHAR(255),
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )"
+        ];
+    }
+
+    public function down(): array
+    {
+        return [
+            "DROP TABLE users"
+        ];
+    }
+}
