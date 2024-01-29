ae2d304dc5308dcfe9c91552615ce3786fcd8dec
AkinoJoey <gakiya213@gmail.com>
2024-01-29 17:15:51 +0900

    Routeクラスを作成

---
+++ src/routing/Route.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace src\routing;
+
+use Closure;
+use src\helpers\Settings;
+
+class Route
+{
+
+    private string $path;
+    /**
+     * @var string[]
+     */
+    private array $middleware;
+    private Closure $callback;
+
+    public function __construct(string $path, callable $callback)
+    {
+        $this->path = $path;
+        // これは、Closure::fromCallable($callable) の代替構文です
+        // クロージャはコールバックをクラスにカプセル化し、そのスコープをカプセル化します。元のスコープの変数を使用するにはuseキーワードを使用します
+        $this->callback = $callback(...);
+    }
+
+    // ルートを作成するための静的関数
+    // (new Route($callback))->setMiddleware(...)の代わりに、Route::create($callback)->setMiddleware(...)を実行できます
+    public static function create(string $path, callable $callback): Route
+    {
+        return new self($path, $callback);
+    }
+
+    public function setMiddleware(array $middleware): Route
+    {
+        $this->middleware = $middleware;
+        return $this;
+    }
+
+    public function getMiddleware(): array
+    {
+        return $this->middleware ?? [];
+    }
+
+    public function getCallback(): Closure
+    {
+        return $this->callback;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    private function getBaseURL(): string
+    {
+        if (!isset($_SERVER)) return $this->getPath();
+
+        $host = $_SERVER['HTTP_HOST'];
+        return sprintf("%s/%s", $host, $this->getPath());
+    }
+
+    private function getSecretKey(): string
+    {
+        return Settings::env("SIGNATURE_SECRET_KEY");
+    }
+
+    public function getSignedURL(array $queryParameters): string
+    {
+        $url = $this->getBaseURL();
+
+        // 組み込み関数のhttp_build_queryを使用すると、 URLパラメータのクエリ文字列を配列から作成することができます。
+        $queryString = http_build_query($queryParameters);
+
+        // HMAC-SHA256を使って署名を作成します。
+        $signature = hash_hmac('sha256', $url . '?' . $queryString, $this->getSecretKey());
+
+        // パーツを組み合わせて値を返します。
+        return sprintf("%s?%s&signature=%s", $url, $queryString, $signature);
+    }
+
+    public function isSignedURLValid(string $url, bool $absolute = true): bool
+    {
+        // URLデータを含む連想配列を返すparse_url組み込み関数を使用して、URLから署名を抽出します。
+        $parsedUrl = parse_url($url);
+        if (!isset($parsedUrl['query'])) return false;
+
+        $queryParams = [];
+
+        // $parsedUrl['query']は、"param1=value1&param2=value2 "の形式でキーと値のペアを返し、parse_strはそれらを配列に挿入するために文字列をパースすることができます。
+        parse_str($parsedUrl['query'], $queryParams);
+
+        if (!isset($queryParams['signature'])) return false;
+
+        $signature = $queryParams['signature'];
+
+        // 検証用URLから署名を削除します。
+        $urlWithoutSignature = str_replace('&signature=' . $signature, '', $url);
+
+        // URL生成時と同じ方法で署名を再作成します。
+        $expectedSignature = hash_hmac('sha256', $urlWithoutSignature, $this->getSecretKey());
+
+        return hash_equals($expectedSignature, $signature);
+    }
+}
--- src/routing/routes.php
+++ src/routing/routes.php
@@ -2,21 +2,110 @@
 
 use src\response\HTTPRenderer;
 use src\response\render\HTMLRenderer;
+use src\types\ValueType;
+use src\helpers\ValidationHelper;
+use src\response\FlashData;
+use src\response\render\RedirectRenderer;
+use src\database\data_access\DAOFactory;
+use src\models\User;
+use src\helpers\Authenticate;
+use src\routing\Route;
 
 return [
-    '' => function (): HTTPRenderer {
+    '' => Route::create('', function (): HTTPRenderer {
         return new HTMLRenderer('page/top');
-    },
-    'guest' =>function () : HTTPRenderer{
+    })->setMiddleware([]),
+    'guest' => Route::create('guest', function (): HTTPRenderer {
         return new HTMLRenderer('page/guest');
-    },
-    'signup' =>function(): HTTPRenderer{
+    })->setMiddleware([]),
+    'signup' => Route::create('signup', function (): HTTPRenderer {
         return new HTMLRenderer('page/signup');
-    },
-    'login' =>function () : HTTPRenderer{
+    })->setMiddleware(['guest']),
+    'form/signup' => Route::create('form/signup', function (): HTTPRenderer {
+        // TODO: エラーのtry-catch
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') throw new Exception('Invalid request method!');
+
+        $required_fields = [
+            'account_name' => ValueType::STRING,
+            'email' => ValueType::EMAIL,
+            'password' => ValueType::PASSWORD,
+            'confirm_password' => ValueType::PASSWORD,
+        ];
+
+        $userDao = DAOFactory::getUserDAO();
+
+        // シンプルな検証
+        $validatedData = ValidationHelper::validateFields($required_fields, $_POST);
+
+        if ($validatedData['confirm_password'] !== $validatedData['password']) {
+            FlashData::setFlashData('error', 'Invalid Password!');
+            return new RedirectRenderer('signup');
+        }
+
+        // Eメールは一意でなければならないので、Eメールがすでに使用されていないか確認します
+        if ($userDao->getByEmail($validatedData['email'])) {
+            FlashData::setFlashData('error', 'Email is already in use!');
+            return new RedirectRenderer('signup');
+        }
+
+        // 新しいUserオブジェクトを作成します
+        $user = new User(
+            accountName: $validatedData['account_name'],
+            email: $validatedData['email'],
+        );
+
+        // データベースにユーザーを作成しようとします
+        $success = $userDao->create($user, $validatedData['password']);
+
+        if (!$success) throw new Exception('Failed to create new user!');
+
+        // ユーザーログイン
+        Authenticate::loginAsUser($user);
+
+        // 期限を30分に設定
+        $lasts = 1 * 60 * 30;
+        $param = [
+            'id' => $user->getId(),
+            'user' => hash('sha256', $user->getEmail()),
+            'expiration' => time() + $lasts
+        ];
+
+        $signedUrl = Route::create('verify/email', function () {
+        })->getSignedURL($param);
+        Authenticate::sendVerificationEmail($user, $signedUrl);
+
+        FlashData::setFlashData('success', 'Account successfully created. Please check your email!');
+        return new RedirectRenderer('login');
+    })->setMiddleware(['guest']),
+    'verify/email' => Route::create('verify/email', function (): HTTPRenderer {
+        $required_fields = [
+            'id' => ValueType::INT,
+            'user' => ValueType::STRING,
+        ];
+
+        $validatedData = ValidationHelper::validateFields($required_fields, $_GET);
+
+        $user = Authenticate::getAuthenticatedUser();
+
+        // ユーザーの詳細とパラメーターが一致しているか確認
+        if ($user === null || $user->getId() !== $validatedData['id'] || hash('sha256', $user->getEmail()) !== $validatedData['user']) {
+            FlashData::setFlashData('error', '無効なURLです。');
+            return new RedirectRenderer('signup');
+        }
+
+        // email_verifiedを更新
+        $user->setEmailVerified(true);
+        $userDao = DAOFactory::getUserDAO();
+        $userDao->update($user);
+
+        FlashData::setFlashData('success', 'Eメールの確認に成功しました。');
+        return new RedirectRenderer('profile');
+    }),
+    'login' => Route::create('login', function (): HTTPRenderer {
         return new HTMLRenderer('page/login');
-    },
-    'profile' => function (): HTTPRenderer {
+    })->setMiddleware(['guest']),
+    'profile' => Route::create('profile', function (): HTTPRenderer {
         return new HTMLRenderer('page/profile');
-    },
+    })->setMiddleware(['auth']),
 ];
