fd6af84e828cf4305ba22d79d211928722f966b8
AkinoJoey <gakiya213@gmail.com>
2024-02-01 17:16:18 +0900

    getProfileDAO関数を追加

--- src/database/data_access/DAOFactory.php
+++ src/database/data_access/DAOFactory.php
@@ -2,7 +2,9 @@
 
 namespace src\database\data_access;
 
+use src\database\data_access\implementations\ProfileDAOImpl;
 use src\database\data_access\implementations\UserDAOImpl;
+use src\database\data_access\interfaces\ProfileDAO;
 use src\database\data_access\interfaces\UserDAO;
 
 class DAOFactory
@@ -10,5 +12,10 @@ class DAOFactory
     public static function getUserDAO(): UserDAO
     {
         return new UserDAOImpl();
+    }
+
+    public static function getProfileDAO(): ProfileDAO
+    {
+        return new ProfileDAOImpl();
     }
 }
