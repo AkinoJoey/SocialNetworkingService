fda2c6daf0f49b667b5581e166ca5fcb53cba219
AkinoJoey <gakiya213@gmail.com>
2024-02-01 17:17:47 +0900

    getByUserId()を修正

--- src/database/data_access/interfaces/ProfileDAO.php
+++ src/database/data_access/interfaces/ProfileDAO.php
@@ -8,6 +8,6 @@ interface ProfileDAO
 {
     public function create(Profile $profile): bool;
     public function getById(int $id): ?Profile;
-    public function getByUserId(string $userId): ?Profile;
+    public function getByUserId(int $userId): ?Profile;
     public function update(Profile $profile): bool;
 }
