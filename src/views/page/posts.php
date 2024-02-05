45e790aefe2427453ffbc4fcba7da843facdc714
AkinoJoey <gakiya213@gmail.com>
2024-02-05 17:35:15 +0900

    動的な振る舞いを追加

--- src/views/page/posts.php
+++ src/views/page/posts.php
@@ -48,8 +48,9 @@
 
             <!-- reply -->
             <div class="w-full border-t border-gray-200">
-                <form action="/form/post" method="POST">
+                <form action="/form/comment" method="POST">
                     <input type="hidden" name="csrf_token" value="<?= src\helpers\CrossSiteForgeryProtection::getToken() ?>" />
+                    <input type="hidden" name="post_id" value="<?= $post->getId()  ?>">
                     <div class="flex p-2">
                         <div>
                             <img class="h-8 w-8 rounded-full" src="https://source.unsplash.com/100x100/?portrait" />
@@ -80,8 +81,8 @@
                         </div>
 
                         <div class="mb-1">
-                            <button type="button" class="rounded-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Default
+                            <button type="submit" class="rounded-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                返信
                             </button>
                         </div>
                     </div>
@@ -89,98 +90,54 @@
             </div>
 
             <!-- comments -->
-            <article class="border-t border-gray-200 bg-white p-6 text-base dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
-                <footer class="mb-2 flex items-center justify-between">
-                    <div class="flex items-center">
-                        <p class="mr-3 inline-flex items-center text-sm font-semibold text-gray-900 dark:text-white">
-                            <img class="mr-2 h-6 w-6 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Michael Gough" />Michael Gough
+            <?php if ($comments) : ?>
+                <?php foreach ($comments as $comment) : ?>
+                    <article class="w-full border-t border-gray-200 bg-white p-6 text-base dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 relative">
+                        <a href="/comments?url=<?= $comment->getUrl()?>" class="absolute h-full w-full top-0 left-0 z-0"></a>
+                        <footer class="mb-2 flex items-center justify-between">
+                            <div class="flex items-center">
+                                <p class="mr-3 inline-flex items-center text-sm font-semibold text-gray-900 dark:text-white">
+                                    <img class="mr-2 h-6 w-6 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Michael Gough" /><?= $comment->getPostedUser()->getAccountName() ?>
+                                </p>
+                                <p class="text-sm text-gray-600 dark:text-gray-400">
+                                    <time pubdate datetime="2022-02-08" title="February 8th, 2022">Feb. 8, 2022</time>
+                                </p>
+                            </div>
+                            <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1" class="inline-flex items-center rounded-lg bg-white p-2 text-center text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-50 dark:bg-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
+                                <svg class="h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                                    <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                                </svg>
+                                <span class="sr-only">Comment settings</span>
+                            </button>
+                            <!-- Dropdown menu -->
+                            <div id="dropdownComment1" class="z-10 hidden w-36 divide-y divide-gray-100 rounded bg-white shadow dark:divide-gray-600 dark:bg-gray-700">
+                                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconHorizontalButton">
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </footer>
+                        <p class="text-gray-500 dark:text-gray-400">
+                            <?= $comment->getContent() ?>
                         </p>
-                        <p class="text-sm text-gray-600 dark:text-gray-400">
-                            <time pubdate datetime="2022-02-08" title="February 8th, 2022">Feb. 8, 2022</time>
-                        </p>
-                    </div>
-                    <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1" class="inline-flex items-center rounded-lg bg-white p-2 text-center text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-50 dark:bg-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
-                        <svg class="h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                            <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-                        </svg>
-                        <span class="sr-only">Comment settings</span>
-                    </button>
-                    <!-- Dropdown menu -->
-                    <div id="dropdownComment1" class="z-10 hidden w-36 divide-y divide-gray-100 rounded bg-white shadow dark:divide-gray-600 dark:bg-gray-700">
-                        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconHorizontalButton">
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
-                            </li>
-                        </ul>
-                    </div>
-                </footer>
-                <p class="text-gray-500 dark:text-gray-400">
-                    Very straight-to-point article. Really worth time reading. Thank
-                    you! But tools are just the instruments for the UX designers. The
-                    knowledge of the design tools are as important as the creation of
-                    the design strategy.
-                </p>
-                <div class="mt-4 flex items-center space-x-4">
-                    <button type="button" class="flex items-center text-sm font-medium text-gray-500 hover:underline dark:text-gray-400">
-                        <svg class="mr-1.5 h-3.5 w-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
-                        </svg>
-                        Reply
-                    </button>
-                </div>
-            </article>
-
-            <article class="border-t border-gray-200 bg-white p-6 text-base dark:bg-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
-                <footer class="mb-2 flex items-center justify-between">
-                    <div class="flex items-center">
-                        <p class="mr-3 inline-flex items-center text-sm font-semibold text-gray-900 dark:text-white">
-                            <img class="mr-2 h-6 w-6 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie Green" />Bonnie Green
-                        </p>
-                        <p class="text-sm text-gray-600 dark:text-gray-400">
-                            <time pubdate datetime="2022-03-12" title="March 12th, 2022">Mar. 12, 2022</time>
-                        </p>
-                    </div>
-                    <button id="dropdownComment3Button" data-dropdown-toggle="dropdownComment3" class="dark:text-gray-40 inline-flex items-center rounded-lg bg-white p-2 text-center text-sm font-medium text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
-                        <svg class="h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                            <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-                        </svg>
-                        <span class="sr-only">Comment settings</span>
-                    </button>
-                    <!-- Dropdown menu -->
-                    <div id="dropdownComment3" class="z-10 hidden w-36 divide-y divide-gray-100 rounded bg-white shadow dark:divide-gray-600 dark:bg-gray-700">
-                        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconHorizontalButton">
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
-                            </li>
-                        </ul>
-                    </div>
-                </footer>
-                <p class="text-gray-500 dark:text-gray-400">
-                    The article covers the essentials, challenges, myths and stages
-                    the UX designer should consider while creating the design
-                    strategy.
-                </p>
-                <div class="mt-4 flex items-center space-x-4">
-                    <button type="button" class="flex items-center text-sm font-medium text-gray-500 hover:underline dark:text-gray-400">
-                        <svg class="mr-1.5 h-3.5 w-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
-                        </svg>
-                        Reply
-                    </button>
-                </div>
-            </article>
+                        <div class="mt-4 flex items-center space-x-4">
+                            <button type="button" class="flex items-center text-sm font-medium text-gray-500 hover:underline dark:text-gray-400">
+                                <svg class="mr-1.5 h-3.5 w-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                                </svg>
+                                Reply
+                            </button>
+                        </div>
+                    </article>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
