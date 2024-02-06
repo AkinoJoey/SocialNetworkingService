71b43eb5f1fe1f658aa1813048ce1f3fdc63ffc6
AkinoJoey <gakiya213@gmail.com>
2024-02-06 17:52:15 +0900

    posts用のファイルを作成

---
+++ src/assets/js/posts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const postLikeBtn = document.getElementById("post_like");
+
+	postLikeBtn.addEventListener("click", function (event) {
+		console.log("test");
+	});
+});
