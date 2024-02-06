8cc9853ebcc39c7822af6da38cbedc15f8641081
AkinoJoey <gakiya213@gmail.com>
2024-02-06 17:52:06 +0900

    postLike用のjsを作成

---
+++ src/assets/js/postLike.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const postLikeBtn = document.getElementById("post_like");
+
+	postLikeBtn.addEventListener("click", function (event) {
+
+        console.log("test");
+	});
+});
