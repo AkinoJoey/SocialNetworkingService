d5d2b65d2c40e8273a3358094299beaa023d0a8c
AkinoJoey <gakiya213@gmail.com>
2024-02-06 17:51:30 +0900

    mainとpostsを作成

---
+++ public/posts.bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/js/posts.js":
+/*!********************************!*\
+  !*** ./src/assets/js/posts.js ***!
+  \********************************/
+/***/ (() => {
+
+eval("document.addEventListener(\"DOMContentLoaded\", function () {\r\n\tconst postLikeBtn = document.getElementById(\"post_like\");\r\n\r\n\tpostLikeBtn.addEventListener(\"click\", function (event) {\r\n\t\tconsole.log(\"test\");\r\n\t});\r\n});\r\n\n\n//# sourceURL=webpack:///./src/assets/js/posts.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/posts.js"]();
+/******/ 	
+/******/ })()
+;
