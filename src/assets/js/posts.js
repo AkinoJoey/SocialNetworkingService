78bec99a1060f4f3faa7fa4770556f59f9722f65
AkinoJoey <gakiya213@gmail.com>
2024-02-07 18:26:27 +0900

    likePostの挙動を追加

--- src/assets/js/posts.js
+++ src/assets/js/posts.js
@@ -1,7 +1,63 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const postLikeBtn = document.getElementById("post_like");
+    const likePostForm = document.getElementById("like_post_form");
+	let like = false;
+	let numberOfPostLikeSpan = document.getElementById("number_of_post_like");
+	let numberOfPostLike = Number(numberOfPostLikeSpan.textContent);
 
-	postLikeBtn.addEventListener("click", function (event) {
-		console.log("test");
+	likePostForm.addEventListener("submit", function (event) {
+		event.preventDefault();
+
+		const formData = new FormData(form);
+
+		if (like) {
+			deleteLikePost(formData);
+
+		} else {
+			likePost(formData);
+		}
 	});
+
+	function likePost(formData) {
+		fetch("form/like-post", {
+			method: "POST",
+			body: formData,
+		})
+			.then((response) => response.json()) 
+			.then((data) => {
+				if (data.status === "success") {
+					like = true;
+					numberOfPostLike += 1;
+					numberOfPostLikeSpan.innerHTML = numberOfPostLike;
+				} else if (data.status === "error") {
+					// ユーザーにエラーメッセージを表示します
+					console.error(data.message);
+					alert("Update failed: " + data.message);
+				}
+			})
+			.catch((error) => {
+				alert("An error occurred. Please try again.");
+			});
+	}
+
+	function deleteLikePost(formData) {
+		fetch("form/like-post", {
+			method: "POST",
+			body: formData,
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.status === "success") {
+					like = false;
+					numberOfPostLike -= 1;
+					numberOfPostLikeSpan.innerHTML = numberOfPostLike;
+				} else if (data.status === "error") {
+					// ユーザーにエラーメッセージを表示します
+					console.error(data.message);
+					alert("Update failed: " + data.message);
+				}
+			})
+			.catch((error) => {
+				alert("An error occurred. Please try again.");
+			});
+	}
 });
