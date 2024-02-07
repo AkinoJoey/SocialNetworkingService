ba1426a8f5d82047fffbbeadf93748c51b2bdbee
AkinoJoey <gakiya213@gmail.com>
2024-02-07 18:26:10 +0900

    getCreatedUserを追加

--- src/views/page/posts.php
+++ src/views/page/posts.php
@@ -6,7 +6,7 @@
                 <img alt="" src="https://source.unsplash.com/100x100/?portrait" class="h-12 w-12 rounded-full object-cover shadow dark:bg-gray-500" />
             </a>
             <div class="flex flex-col space-y-1">
-                <a rel="noopener noreferrer" href="#" class="text-sm font-semibold"><?= $postedUser->getAccountName() ?></a>
+                <a rel="noopener noreferrer" href="#" class="text-sm font-semibold"><?= $post->getCreatedUser()->getAccountName() ?></a>
                 <span class="text-xs dark:text-gray-400">4 hours ago</span>
             </div>
         </div>
@@ -25,13 +25,16 @@
                     </svg>
                     <span>30</span>
                 </button>
-                <button type="button" id="post_like" class="z-50 flex items-center space-x-1.5 p-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Number of likes" class="h-4 w-4 fill-current dark:text-violet-400">
-                        <path d="M126.638,202.672H51.986a24.692,24.692,0,0,0-24.242,19.434,487.088,487.088,0,0,0-1.466,206.535l1.5,7.189a24.94,24.94,0,0,0,24.318,19.78h74.547a24.866,24.866,0,0,0,24.837-24.838V227.509A24.865,24.865,0,0,0,126.638,202.672ZM119.475,423.61H57.916l-.309-1.487a455.085,455.085,0,0,1,.158-187.451h61.71Z"></path>
-                        <path d="M494.459,277.284l-22.09-58.906a24.315,24.315,0,0,0-22.662-15.706H332V173.137l9.573-21.2A88.117,88.117,0,0,0,296.772,35.025a24.3,24.3,0,0,0-31.767,12.1L184.693,222.937V248h23.731L290.7,67.882a56.141,56.141,0,0,1,21.711,70.885l-10.991,24.341L300,169.692v48.98l16,16H444.3L464,287.2v9.272L396.012,415.962H271.07l-86.377-50.67v37.1L256.7,444.633a24.222,24.222,0,0,0,12.25,3.329h131.6a24.246,24.246,0,0,0,21.035-12.234L492.835,310.5A24.26,24.26,0,0,0,496,298.531V285.783A24.144,24.144,0,0,0,494.459,277.284Z"></path>
-                    </svg>
-                    <span><?= $NumberOfPostLike ?></span>
-                </button>
+                <form id="like_post_form">
+                    <button type="submit" name="post_like_btn" id="post_like_btn" class="z-50 flex items-center space-x-1.5 p-1">
+                        <input type="hidden" name="post_id" value="<?= $post->getId() ?>">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Number of likes" class="h-4 w-4 fill-current dark:text-violet-400">
+                            <path d="M126.638,202.672H51.986a24.692,24.692,0,0,0-24.242,19.434,487.088,487.088,0,0,0-1.466,206.535l1.5,7.189a24.94,24.94,0,0,0,24.318,19.78h74.547a24.866,24.866,0,0,0,24.837-24.838V227.509A24.865,24.865,0,0,0,126.638,202.672ZM119.475,423.61H57.916l-.309-1.487a455.085,455.085,0,0,1,.158-187.451h61.71Z"></path>
+                            <path d="M494.459,277.284l-22.09-58.906a24.315,24.315,0,0,0-22.662-15.706H332V173.137l9.573-21.2A88.117,88.117,0,0,0,296.772,35.025a24.3,24.3,0,0,0-31.767,12.1L184.693,222.937V248h23.731L290.7,67.882a56.141,56.141,0,0,1,21.711,70.885l-10.991,24.341L300,169.692v48.98l16,16H444.3L464,287.2v9.272L396.012,415.962H271.07l-86.377-50.67v37.1L256.7,444.633a24.222,24.222,0,0,0,12.25,3.329h131.6a24.246,24.246,0,0,0,21.035-12.234L492.835,310.5A24.26,24.26,0,0,0,496,298.531V285.783A24.144,24.144,0,0,0,494.459,277.284Z"></path>
+                        </svg>
+                        <span id="number_of_post_like"><?= $NumberOfPostLike ?></span>
+                    </button>
+                </form>
             </div>
 
             <!-- reply -->
