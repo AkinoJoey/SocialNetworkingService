ae1cac0ff1400d02e3c9881bc321768f5276ab21
AkinoJoey <gakiya213@gmail.com>
2024-02-09 17:37:57 +0900

    サイドバーのテキストを修正

--- src/views/layout/sidebar.php
+++ src/views/layout/sidebar.php
@@ -16,7 +16,7 @@
 			<aside class="fixed left-0 top-0 z-40 h-screen w-14 -translate-x-full transition-transform sm:translate-x-0 lg:w-40" aria-label="Sidebar">
 				<div class="h-full overflow-y-auto bg-gray-50 px-3 py-4 dark:bg-gray-800">
 					<a href="/" class="mb-5 flex items-center ps-2.5">
-						<span class="invisible self-center whitespace-nowrap text-xl font-semibold dark:text-white lg:visible">ten</span>
+						<span class="hidden self-center whitespace-nowrap text-xl font-semibold dark:text-white lg:block">ten</span>
 					</a>
 					<ul class="space-y-2 font-medium">
 						<li>
@@ -24,7 +24,7 @@
 								<svg class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
 									<path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
 								</svg>
-								<span class="invisible ms-3 flex-1 whitespace-nowrap lg:visible">ホーム</span>
+								<span class="hidden ms-3 flex-1 whitespace-nowrap lg:block">ホーム</span>
 							</a>
 						</li>
 						<li>
@@ -32,7 +32,7 @@
 								<svg class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
 									<path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
 								</svg>
-								<span class="invisible ms-3 flex-1 whitespace-nowrap lg:visible">通知</span>
+								<span class="hidden ms-3 flex-1 whitespace-nowrap lg:block">通知</span>
 							</a>
 						</li>
 						<li>
@@ -40,7 +40,7 @@
 								<svg class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
 									<path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
 								</svg>
-								<span class="invisible ms-3 flex-1 whitespace-nowrap lg:visible">メッセージ</span>
+								<span class="hidden ms-3 flex-1 whitespace-nowrap lg:block">メッセージ</span>
 								<!-- <span
 								class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300"
 								>3</span
@@ -52,7 +52,7 @@
 								<svg class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
 									<path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
 								</svg>
-								<span class="invisible ms-3 flex-1 whitespace-nowrap lg:visible">プロフィール</span>
+								<span class="hidden ms-3 flex-1 whitespace-nowrap lg:block">プロフィール</span>
 							</a>
 						</li>
 						<li>
@@ -61,7 +61,7 @@
 								<svg class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2" />
 								</svg>
-								<span class="ms-3 flex-1 whitespace-nowrap invisible lg:visible">ログアウト</span>
+								<span class="ms-3 flex-1 whitespace-nowrap hidden lg:block">ログアウト</span>
 							</a>
 						</li>
 						<li>
@@ -69,7 +69,7 @@
 								<svg class="w-[25px] h-[25x] flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M5 12h14m-7 7V5" />
 								</svg>
-								<span class="ms-3 flex-1 whitespace-nowrap">作成</span>
+								<span class="ms-3 flex-1 whitespace-nowrap hidden lg:block">作成</span>
 							</button>
 						</li>
 					</ul>
