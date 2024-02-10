1f1bf3c86c572b91969cd23943ae78a278e56c0b
AkinoJoey <gakiya213@gmail.com>
2024-02-10 19:37:13 +0900

    getFollowingUserIdListとgetFollowerUserIdListの戻り値を?arrayに変更

--- src/database/data_access/interfaces/FollowDAO.php
+++ src/database/data_access/interfaces/FollowDAO.php
@@ -8,7 +8,7 @@ interface FollowDAO
 {
     public function create(Follow $follow): bool;
     public function delete(int $followingUserId, int $followerUserId): bool;
-    public function getFollowingUserIdList(int $userId) : array;
-    public function getFollowerUserIdList(int $userId) : array;
+    public function getFollowingUserIdList(int $userId) : ?array;
+    public function getFollowerUserIdList(int $userId) : ?array;
     public function isFollow(int $following_user_id, int $follower_user_id): bool;
 }
