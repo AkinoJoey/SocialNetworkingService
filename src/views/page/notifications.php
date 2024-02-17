f4e93778a821d62f67ee757d23e17490b44e3b9f
AkinoJoey <gakiya213@gmail.com>
2024-02-17 19:34:19 +0900

    notification_typeによって分岐させる挙動を追加

--- src/views/page/notifications.php
+++ src/views/page/notifications.php
@@ -1,8 +1,7 @@
 <?php
+
 use src\types\NotificationType;
 use src\database\data_access\DAOFactory;
-
-$userDao = DAOFactory::getUserDAO();
 ?>
 <!-- notification -->
 <div class="container mx-auto mb-14 flex items-center justify-center p-4">
@@ -12,7 +11,34 @@ $userDao = DAOFactory::getUserDAO();
         </div>
         <div class="divide-y divide-gray-100 dark:divide-gray-700">
             <?php foreach ($notifications as $notification) : ?>
-                <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
+                <?php
+                $userDao = DAOFactory::getUserDAO();
+                $sourceUser = $userDao->getById($notification->getSourceId());
+
+                switch ($notification->getNotificationType()) {
+                    case NotificationType::POST_LIKE->value:
+                        $notificationMessage =  'さんがあなたの投稿にいいねしました';
+                        $postDao = DAOFactory::getPostDAO();
+                        $href = '/posts?url=' . $postDao->getById($notification->getPostId())->getUrl();
+                        break;
+                    case NotificationType::COMMENT->value;
+                        $notificationMessage =  'さんがあなたの投稿にコメントをしました';
+                        $commentDao = DAOFactory::getCommentDAO();
+                        $href = '/comments?url=' . $commentDao->getById($notification->getCommentId())->getUrl();
+                        break;
+                    case NotificationType::FOLLOW->value;
+                        $notificationMessage =  'さんがあなたをフォローしました';
+                        $href = '/profile?username=' . $sourceUser->getUsername();
+                        break;
+                    case NotificationType::DM->value;
+                        $notificationMessage =  'さんからあなたにメッセージが届いています';
+                        $dmThreadDao = DAOFactory::getDmThreadDAO();
+                        $href = '/direct?url=' . $dmThreadDao->getByUserIds($user->getId(), $notification->getSourceId());
+                        break;
+                }
+
+                ?>
+                <a href="<?= $href ?>" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                     <div class="flex w-full items-center">
                         <div class="mr-2">
                             <img class="rounded-full" src="https://source.unsplash.com/100x100/?portrait" alt="Jese image" />
@@ -20,24 +46,8 @@ $userDao = DAOFactory::getUserDAO();
                         <div class="w-3/4">
                             <div class="flex h-full flex-col justify-center">
                                 <div class="mb-1.5 text-sm text-gray-500 dark:text-gray-400">
-                                    <span class="font-semibold text-gray-900 dark:text-white"><?= $userDao->getById($notification->getUserId())->getAccountName() ?></span>
-                                    <?php
-                                        switch($notification->getNotificationType()){
-                                            case NotificationType::POST_LIKE->value:
-                                                echo 'さんがあなたの投稿にいいねしました';
-                                                break;
-                                            case NotificationType::COMMENT->value;
-                                                echo 'さんがあなたの投稿にコメントをしました';
-                                                break;
-                                            case NotificationType::FOLLOW->value;
-                                                echo 'さんがあなたをフォローしました';
-                                                break;
-                                            case NotificationType::DM->value;
-                                                echo 'さんからあなたにメッセージが届いています';
-                                                break;
-                                        }
-                                    ?>
-                                    : "Hey, what's up? All set for the presentation?"
+                                    <span class="font-semibold text-gray-900 dark:text-white"><?= $sourceUser->getAccountName() ?></span>
+                                    <?= $notificationMessage ?>
                                 </div>
                                 <div class="text-xs text-blue-600 dark:text-blue-500">
                                     a few moments ago
