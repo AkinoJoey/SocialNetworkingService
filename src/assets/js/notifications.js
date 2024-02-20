365f974c92a44e800bc98108ea156bf319c5a279
AkinoJoey <gakiya213@gmail.com>
2024-02-20 16:51:15 +0900

    notification用のjsを追加

---
+++ src/assets/js/notifications.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+	let notifications = document.querySelectorAll(".notification");
+
+	notifications.forEach(function (notification) {
+		notification.addEventListener("click", function (e) {
+			e.preventDefault();
+
+			let notificationId = notification.getAttribute("data-notification-id");
+			let isRead = notification.getAttribute("data-notification-isRead");
+			let formData = new FormData();
+			formData.append("notification_id", notificationId);
+			formData.append("csrf_token", csrfToken);
+
+			if (isRead === "1") {
+				window.location.href = notification.href;
+			} else {
+				fetch("/update-isRead", {
+					method: "POST",
+					body: formData,
+				})
+					.then((response) => response.json())
+					.then((data) => {
+						if (data.status === "success") {
+							window.location.href = notification.href;
+						} else if (data.status === "error") {
+							alert(data.message);
+						}
+					})
+					.catch((error) => {
+						alert("An error occurred. Please try again.");
+					});
+			}
+		});
+	});
+});
