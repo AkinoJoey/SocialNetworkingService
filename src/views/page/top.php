b4d84b3ee62fcc54c2560e64b31c522c948ac78c
AkinoJoey <gakiya213@gmail.com>
2024-02-20 17:54:05 +0900

    不要なアイコンを削除

--- src/views/page/top.php
+++ src/views/page/top.php
@@ -19,18 +19,6 @@
 				</p>
 			</div>
 			<div class="flex flex-wrap justify-between">
-				<div class="space-x-2">
-					<button aria-label="Share this post" type="button" class="p-2 text-center z-40">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-4 w-4 fill-current dark:text-violet-400">
-							<path d="M404,344a75.9,75.9,0,0,0-60.208,29.7L179.869,280.664a75.693,75.693,0,0,0,0-49.328L343.792,138.3a75.937,75.937,0,1,0-13.776-28.976L163.3,203.946a76,76,0,1,0,0,104.108l166.717,94.623A75.991,75.991,0,1,0,404,344Zm0-296a44,44,0,1,1-44,44A44.049,44.049,0,0,1,404,48ZM108,300a44,44,0,1,1,44-44A44.049,44.049,0,0,1,108,300ZM404,464a44,44,0,1,1,44-44A44.049,44.049,0,0,1,404,464Z"></path>
-						</svg>
-					</button>
-					<button aria-label="Bookmark this post" type="button" class="p-2 z-40">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-4 w-4 fill-current dark:text-violet-400">
-							<path d="M424,496H388.75L256.008,381.19,123.467,496H88V16H424ZM120,48V456.667l135.992-117.8L392,456.5V48Z"></path>
-						</svg>
-					</button>
-				</div>
 				<div class="flex space-x-2 text-sm dark:text-gray-400">
 					<button type="button" class="flex items-center space-x-1.5 p-1 z-40">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Number of comments" class="h-4 w-4 fill-current dark:text-violet-400">
