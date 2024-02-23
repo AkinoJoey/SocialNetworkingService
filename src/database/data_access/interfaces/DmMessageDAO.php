51c833e03e9972d17b6c61ad653ef3e543700982
AkinoJoey <gakiya213@gmail.com>
2024-02-23 18:34:23 +0900

    getMessageListを追加

--- src/database/data_access/interfaces/DmMessageDAO.php
+++ src/database/data_access/interfaces/DmMessageDAO.php
@@ -9,4 +9,5 @@ interface DmMessageDAO{
     public function delete(int $id) : bool;
     public function getById(int $id) : DmMessage;
     public function getOneHundredByDmThreadId(int $dmThreadId) : array;
+    public function getMessageList(int $userId, int $limit = 100): array;
 }
