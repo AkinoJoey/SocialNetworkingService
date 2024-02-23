3230b889efea9809947d455268e2763a96d5b245
AkinoJoey <gakiya213@gmail.com>
2024-02-23 18:33:11 +0900

    messageListをforeachで回すようにした

--- src/views/page/messages.php
+++ src/views/page/messages.php
@@ -5,17 +5,21 @@
             メッセージ
         </div>
         <div class="divide-y divide-gray-100 dark:divide-gray-700">
-            <?php foreach ($threads as $thread) : ?>
-                <a href="direct?url=<?= $thread->getUrl() ?>" data-thread-id="<?= $thread->getId() ?>" class="notification flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:cursor-pointer">
+            <?php foreach ($messageList as $message) : ?>
+                <a href="direct?url=<?= $message->getUrl() ?>" data-message-id="<?= $message->getId() ?>" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:cursor-pointer">
                     <div class="flex w-full items-center">
                         <div class="mr-2">
                             <img class="rounded-full" src="https://source.unsplash.com/100x100/?portrait" alt="Jese image" />
                         </div>
                         <div class="w-3/4">
                             <div class="flex h-full flex-col justify-center">
-                                <div class="mb-1.5 text-sm text-gray-950 dark:text-gray-400">
-                                    <span class="font-semibold text-gray-900 dark:text-white"><?= $sourceUser->getAccountName() ?></span>
+                                <div class="mb-1.5 text-sm">
+                                    <span class="font-semibold text-gray-900 dark:text-white"><?= $message->getFromUserAccountName() ?></span>
                                     さんとのメッセージ
+                                    <p class="text-sm mt-1 text-gray-600 dark:text-gray-400"><?= ($message->getSenderUserId() == $user->getId()) ? 'You' : $message->getFromUserAccountName() ?>: <?= $message->getMessage() ?></p>
+                                </div>
+                                <div class="text-xs text-blue-600 dark:text-blue-500">
+                                    a few moments ago
                                 </div>
                             </div>
                         </div>
@@ -25,9 +29,3 @@
         </div>
     </div>
 </div>
-
-
-<script>
-    const csrfToken = "<?= src\helpers\CrossSiteForgeryProtection::getToken(); ?>"
-</script>
-<script src="/notifications.bundle.js"></script>
