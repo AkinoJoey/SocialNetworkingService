4468e587b02bdf80479e55995d7ee7011254678d
AkinoJoey <gakiya213@gmail.com>
2024-02-25 19:22:16 +0900

    deleteとgetByUrlにuserIdを追加

--- src/database/data_access/interfaces/PostDAO.php
+++ src/database/data_access/interfaces/PostDAO.php
@@ -9,8 +9,8 @@ interface PostDAO
     public function create(Post $post): bool;
     public function getById(int $id): ?Post;
     public function getByUserId(int $userId): ?Post;
-    public function delete(int $id): bool;
+    public function delete(int $id, int $userId): bool;
     public function getTwentyPosts(int $userId, int $offset) : array;
-    public function getByUrl(string $url): ?Post;
+    public function getByUrl(string $url, int $userId): ?Post;
     public function getPostsByFollowedUsers(array $followedUserIds, int $userId, int $offset, int $limit = 20): array;
 }
