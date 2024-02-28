0fa08915dfe7e7bb1491db95d5ee2677477825f5
AkinoJoey <gakiya213@gmail.com>
2024-02-28 18:16:12 +0900

    post modalをコンポーネント化

---
+++ src/views/components/new_post.php
@@ -0,0 +1,45 @@
+<!-- post modal -->
+<div id="post_modal" class="fixed left-0 right-0 top-0 z-50 hidden h-screen w-full items-center justify-center">
+    <div class="w-full max-h-96 sm:max-h-[570px] overflow-auto rounded-xl bg-white sm:w-2/3 lg:w-2/5">
+        <div class="flex items-center justify-between border-b px-5 py-3 text-blue-400">
+            <button data-modal-hide="post_modal" class="rounded-full px-3 py-1 hover:bg-blue-100">
+                <i class="fas fa-times text-xl"></i>
+            </button>
+
+            <p class="inline cursor-pointer rounded-full px-4 py-3 font-bold hover:bg-blue-100">
+                未送信ポスト
+            </p>
+        </div>
+        <form id="create-post-form">
+            <input type="hidden" name="csrf_token" value="<?= src\helpers\CrossSiteForgeryProtection::getToken() ?>">
+            <div class="flex p-4">
+                <div>
+                    <img class="w-14 rounded-full" src="https://source.unsplash.com/100x100/?portrait" />
+                </div>
+
+                <div class="ml-3 flex w-full flex-col">
+                    <textarea id="content" name="content" placeholder="今何してる?" class="h-32 w-full resize-none rounded-xl text-xl border-none focus:ring-0" maxlength="280"></textarea>
+                    <input class="hidden" id="file-input" type="file" name="media" accept="image/png, image/gif, image/jpeg, image/jpg, image/webp, video/mp4, video/mov">
+                    <div class="w-full flex justify-center rounded-full relative" id="previewContainer">
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex items-center justify-between border-t px-4 py-6 text-blue-400">
+                <div class="flex pl-12 text-2xl">
+                    <div id="file-input-icon" class="flex cursor-pointer items-center justify-center rounded-full p-3 hover:bg-blue-100">
+                        <i class="fas fa-image"></i>
+                    </div>
+                    <div id="calender" class="flex cursor-pointer items-center justify-center rounded-full p-3 hover:bg-blue-100">
+                        <i class="fas fa-calendar-alt"></i>
+                    </div>
+                </div>
+                <div>
+                    <button id="post-submit" type="submit" class="inline cursor-pointer rounded-full bg-blue-500 px-4 py-3 font-bold text-white hover:opacity-75 btn btn-disabled">
+                        投稿
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
--- src/views/layout/sidebar.php
+++ src/views/layout/sidebar.php
@@ -82,7 +82,7 @@
 							</a>
 						</li>
 						<li>
-							<button data-modal-target="post_modal" data-modal-show="post_modal" class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+							<button id="open_post_modal" data-modal-target="post_modal" data-modal-show="post_modal" class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
 								<svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M5 12h14m-7 7V5" />
 								</svg>
@@ -118,7 +118,7 @@
 						Bookmark
 						<div class="tooltip-arrow" data-popper-arrow></div>
 					</div>
-					<button data-modal-target="post_modal" data-modal-show="post_modal" data-tooltip-target="tooltip-post" type="button" class="group inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 dark:hover:bg-gray-800">
+					<button id="open_post_modal" data-modal-target="post_modal" data-modal-show="post_modal" data-tooltip-target="tooltip-post" type="button" class="group inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 dark:hover:bg-gray-800">
 						<svg class="mb-1 h-5 w-5 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
 							<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
 						</svg>
@@ -154,58 +154,7 @@
 				</div>
 			</div>
 
-			<!-- post modal -->
-			<div id="post_modal" class="fixed left-0 right-0 top-0 z-50 hidden h-screen w-full items-center justify-center">
-				<div class="w-full rounded-xl bg-white sm:w-2/3 lg:w-1/2">
-					<div class="flex items-center justify-between border-b px-5 py-3 text-blue-400">
-						<button data-modal-hide="post_modal" class="rounded-full px-4 py-3 hover:bg-blue-100">
-							<i class="fas fa-times text-xl"></i>
-						</button>
-
-						<p class="inline cursor-pointer rounded-full px-4 py-3 font-bold hover:bg-blue-100">
-							未送信ポスト
-						</p>
-					</div>
-					<form action="/form/new" method="POST">
-						<input type="hidden" name="csrf_token" value="<?= src\helpers\CrossSiteForgeryProtection::getToken() ?>">
-						<div class="flex p-4">
-							<div>
-								<img class="w-14 rounded-full" src="https://source.unsplash.com/100x100/?portrait" />
-							</div>
-
-							<div class="ml-3 flex w-full flex-col">
-								<textarea id="content" name="content" placeholder="What's happening?" class="h-32 w-full resize-none rounded-xl border-gray-200 text-xl outline-none"></textarea>
-							</div>
-						</div>
-
-						<div class="flex items-center justify-between border-t px-4 py-6 text-blue-400">
-							<div class="flex pl-12 text-2xl">
-								<div class="flex cursor-pointer items-center justify-center rounded-full p-3 hover:bg-blue-100">
-									<i class="fas fa-image"></i>
-								</div>
-
-								<div class="flex cursor-pointer items-center justify-center rounded-full p-3 hover:bg-blue-100">
-									<i class="fas fa-poll-h"></i>
-								</div>
-
-								<div class="flex cursor-pointer items-center justify-center rounded-full p-3 hover:bg-blue-100">
-									<i class="fas fa-smile"></i>
-								</div>
-
-								<div class="flex cursor-pointer items-center justify-center rounded-full p-3 hover:bg-blue-100">
-									<i class="fas fa-calendar-alt"></i>
-								</div>
-							</div>
-
-							<div>
-								<button type="submit" class="inline cursor-pointer rounded-full bg-blue-500 px-4 py-3 font-bold text-white">
-									Tweet
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
+			<?php include(__DIR__ . '/../components/new_post.php') ?>
 		<?php endif; ?>
 
 
