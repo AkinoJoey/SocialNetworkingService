15146fb6a573a27f5ce80819db9a493ea3a304e0
AkinoJoey <gakiya213@gmail.com>
2024-02-29 14:48:35 +0900

    assets/jsをcontentに追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/views/*/*.{html,js,php}", "./node_modules/flowbite/**/*.js"],
+	content: ["./src/views/*/*.{html,js,php}", "./node_modules/flowbite/**/*.js", "./src/assets/js/*.js"],
 	theme: {
 		extend: {
 			animation: {
