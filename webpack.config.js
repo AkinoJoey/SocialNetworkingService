fd2ffb5fb76d836e5c250ba0b6dad53dffa1767a
AkinoJoey <gakiya213@gmail.com>
2024-02-29 14:48:49 +0900

    loggedInUserをentryに追加

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
 		profile: "./src/assets/js/profile.js",
 		direct: "./src/assets/js/directAsync.js",
 		notifications: "./src/assets/js/notifications.js",
-		top: "./src/assets/js/top.js"
+		top: "./src/assets/js/top.js",
+		loggedInUser: "./src/assets/js/postAndLogoutModal.js"
 	},
 	output: {
 		path: path.resolve(__dirname, "public"),
