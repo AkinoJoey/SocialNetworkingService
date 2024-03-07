4163fca9bd014c1546e2429885ebe45d9ee1e7b5
AkinoJoey <gakiya213@gmail.com>
2024-03-07 17:51:32 +0900

    見た目を修正

--- src/views/components/schedule.php
+++ src/views/components/schedule.php
@@ -13,12 +13,15 @@
             </div>
             <!-- Modal body -->
             <div class="py-12 space-y-4 flex justify-center">
-                <input class="rounded-lg flatpickr" type="text"  placeholder="日付を入力" readonly>
+                <div class="flatpickr input-group">
+                    <input data-input class="border-gray-200 rounded-lg" type="text" placeholder="日付を入力" readonly>
+                    <button data-clear id="clear" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">クリア</button>
+                </div>
+
             </div>
             <!-- Modal footer -->
             <div class="flex justify-end items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button data-modal-hide="schedule_modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">予約する</button>
-                <button data-modal-hide="schedule_modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">キャンセル</button>
+                <button id="reserve_button" data-modal-hide="schedule_modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">決定</button>
             </div>
         </div>
     </div>
