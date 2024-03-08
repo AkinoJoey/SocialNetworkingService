ee3bb7e2b52709480bfe18225b7d0247268f2e7c
AkinoJoey <gakiya213@gmail.com>
2024-03-08 16:14:10 +0900

    cronから実行するためのスクリプトを作成

---
+++ cron/post_scheduled.php
@@ -0,0 +1,20 @@
+<?php
+# TODO: cron
+# cronでは作業ディレクトリがホームディレクトリになるから、作業ディレクトリをcronディレクトリに変更
+# chdir(__DIR__  . "/..");
+
+$output = [];
+$result = exec("php console po-sc", $output);
+
+// エラーが起きた時間を把握するため、エラーの場合だけerror_log.txtに記録
+$messageIndex = 2;
+
+if($output[$messageIndex] === 'Error occurred.'){
+    // 不要な1行目を削除
+    array_shift($output);
+
+    date_default_timezone_set('Asia/Tokyo');
+    echo date("Y-m-d H:i:s" . "\n");
+
+    if ($result) print_r($output);
+}
