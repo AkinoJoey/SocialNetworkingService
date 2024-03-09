803488b3b8872e44ee87265922d77af0dbc769ff
AkinoJoey <gakiya213@gmail.com>
2024-03-09 13:06:03 +0900

    tabのデフォルトの挙動を追加

--- src/views/components/timeline_tab.php
+++ src/views/components/timeline_tab.php
@@ -1,10 +1,23 @@
-<div class="z-40 fixed top-0 -translate-x-1/2 left-1/2 w-full">
-    <div class="grid max-w-xs grid-cols-2 gap-1 p-1 mx-auto my-2 bg-gray-100 rounded-lg dark:bg-gray-600" role="group">
-        <button type="button" class="px-5 py-1.5 text-xs font-medium text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900 rounded-lg">
-            フォロワー
-        </button>
-        <button type="button" class="px-5 py-1.5 text-xs font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg">
-            トレンド
-        </button>
+<?php if ($tabActive === 'trend') : ?>
+    <div class="z-40 fixed top-0 -translate-x-1/2 left-1/2 w-full">
+        <div class="grid max-w-xs grid-cols-2 gap-1 p-1 mx-auto my-2 bg-gray-100 rounded-lg dark:bg-gray-600" role="group">
+            <button data-tab="trend" type="button" class="tab px-5 py-1.5 text-xs font-medium text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900 rounded-lg">
+                トレンド
+            </button>
+            <button data-tab="following" type="button" class="tab px-5 py-1.5 text-xs font-medium  rounded-lg">
+                フォロー中
+            </button>
+        </div>
     </div>
-</div>
+<?php else : ?>
+    <div class="z-40 fixed top-0 -translate-x-1/2 left-1/2 w-full">
+        <div class="grid max-w-xs grid-cols-2 gap-1 p-1 mx-auto my-2 bg-gray-100 rounded-lg dark:bg-gray-600" role="group">
+            <button data-tab="trend" type="button" class="tab px-5 py-1.5 text-xs font-medium text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700 rounded-lg">
+                トレンド
+            </button>
+            <button data-tab="following" type="button" class="tab px-5 py-1.5 text-xs font-medium text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900 rounded-lg">
+                フォロー中
+            </button>
+        </div>
+    </div>
+<?php endif; ?>
