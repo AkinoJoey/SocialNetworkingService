c440c3ea0a6b8a5d265df276c2229c5aa85fc9d8
AkinoJoey <gakiya213@gmail.com>
2024-03-09 13:06:17 +0900

    timeline_containerを追加

--- src/views/page/top.php
+++ src/views/page/top.php
@@ -3,9 +3,11 @@
 
 <!-- post -->
 <div class="container mx-auto mb-14 flex flex-col items-center justify-center p-4">
-	<?php foreach ($posts as $post) : ?>
-		<?php include(__DIR__ . '/../components/post_card.php') ?>
-	<?php endforeach; ?>
+	<div id="timeline_container">
+		<?php foreach ($posts as $post) : ?>
+			<?php include(__DIR__ . '/../components/post_card.php') ?>
+		<?php endforeach; ?>
+	</div>
 </div>
 
 <?php include(__DIR__ . '/../components/alert_modal.php') ?>
