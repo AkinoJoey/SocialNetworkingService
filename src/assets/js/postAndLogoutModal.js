4c07b3cecfd00bb0ab3ea2295a66707f0a259363
AkinoJoey <gakiya213@gmail.com>
2024-03-16 19:32:49 +0900

    リファクタリング

--- src/assets/js/postAndLogoutModal.js
+++ src/assets/js/postAndLogoutModal.js
@@ -20,62 +20,62 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	// 画像アイコンをクリックしたとき
 	fileInputIcon.addEventListener("click", function () {
-		fileInput.addEventListener("change", function (event) {
-			let file = event.target.files[0]; // 最初のファイルを取得
+		fileInput.click();
+	});
 
-			if (file) {
-				let reader = new FileReader(); // ファイルリーダーオブジェクトを作成
+	fileInput.addEventListener("change", function (event) {
+		let file = event.target.files[0]; // 最初のファイルを取得
 
-				reader.onload = function (e) {
-					previewContainer.innerHTML = ""; // 以前のプレビューをクリア
-					let previewElement;
+		if (file) {
+			let reader = new FileReader(); // ファイルリーダーオブジェクトを作成
 
-					if (file.type.startsWith("image/")) {
-						// 画像の場合
-						previewElement = document.createElement("img");
-					} else if (file.type.startsWith("video/")) {
-						// 動画の場合
-						previewElement = document.createElement("video");
-						previewElement.controls = true; // コントロールバーを表示する
-						previewElement.autoplay = false; // 自動再生を無効にする
-						previewElement.muted = true; // 音声をミュートにする（任意）
-					}
+			reader.onload = function (e) {
+				previewContainer.innerHTML = ""; // 以前のプレビューをクリア
+				let previewElement;
 
-					previewElement.src = e.target.result;
-					previewElement.classList.add("w-full");
+				if (file.type.startsWith("image/")) {
+					// 画像の場合
+					previewElement = document.createElement("img");
+				} else if (file.type.startsWith("video/")) {
+					// 動画の場合
+					previewElement = document.createElement("video");
+					previewElement.controls = true; // コントロールバーを表示する
+					previewElement.autoplay = false; // 自動再生を無効にする
+					previewElement.muted = true; // 音声をミュートにする（任意）
+				}
 
-					let removeBtn = document.createElement("button");
-					removeBtn.classList.add(
-						"rounded-full",
-						"px-2",
-						"hover:opacity-75",
-						"absolute",
-						"top-0",
-						"right-0",
-						"text-black",
-						"bg-white",
-					);
-					removeBtn.innerHTML = `
+				previewElement.src = e.target.result;
+				previewElement.classList.add("w-full");
+
+				let removeBtn = document.createElement("button");
+				removeBtn.classList.add(
+					"rounded-full",
+					"px-2",
+					"hover:opacity-75",
+					"absolute",
+					"top-0",
+					"right-0",
+					"text-black",
+					"bg-white",
+				);
+				removeBtn.innerHTML = `
 				<i class="fas fa-times text-md"></i>
 				`;
-					removeBtn.addEventListener("click", function () {
-						previewContainer.removeChild(previewElement);
-						previewContainer.removeChild(removeBtn);
-						fileInput.value = "";
-						checkForm();
-					});
-
-					previewContainer.appendChild(previewElement);
-					previewContainer.appendChild(removeBtn);
+				removeBtn.addEventListener("click", function () {
+					previewContainer.removeChild(previewElement);
+					previewContainer.removeChild(removeBtn);
+					fileInput.value = "";
 					checkForm();
-				};
+				});
 
-				reader.readAsDataURL(file); // ファイルの内容をBase64エンコードして読み込む
-			}
-			checkForm();
-		});
+				previewContainer.appendChild(previewElement);
+				previewContainer.appendChild(removeBtn);
+				checkForm();
+			};
 
-		fileInput.click();
+			reader.readAsDataURL(file); // ファイルの内容をBase64エンコードして読み込む
+		}
+		checkForm();
 	});
 
 	let today = new Date();
