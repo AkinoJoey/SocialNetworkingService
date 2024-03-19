b8cb418ef0eddeeeb6a731eddc4ad067490d5b4c
AkinoJoey <gakiya213@gmail.com>
2024-03-19 14:21:27 +0900

    削除ボタンの実装

--- src/assets/js/scheduledPosts.js
+++ src/assets/js/scheduledPosts.js
@@ -1,29 +1,32 @@
 import { Modal } from "flowbite";
+import { setupAlertModals } from "./setupAlertModals";
 
 document.addEventListener("DOMContentLoaded", function () {
 	let postModalEl = document.getElementById("post_modal");
 	let postModal = new Modal(postModalEl);
 	let scheduledPosts = document.querySelectorAll(".scheduled-post");
-	let deleteScheduledPosts = document.querySelectorAll(
-		".delete-scheduled-post",
-	);
 	let newPostContent = document.getElementById("content");
 
 	scheduledPosts.forEach(function (scheduledPost) {
 		scheduledPost.addEventListener("click", function () {
 			let scheduledPostContent = scheduledPost.querySelector(
 				".scheduled-post-content",
-            ).textContent;
-            let scheduledAt = scheduledPost.querySelector(".scheduled-at").textContent;
+			).textContent;
+			let scheduledAt =
+				scheduledPost.querySelector(".scheduled-at").textContent;
 
 			postModal.show();
 
 			let scheduledAtContainer = document.getElementById(
 				"scheduledAt_container",
 			);
-            newPostContent.value = scheduledPostContent;
-            scheduledAtContainer.innerHTML = scheduledAt;
-            console.log(getScheduledAt());
+			newPostContent.value = scheduledPostContent;
+			scheduledAtContainer.innerHTML = scheduledAt;
 		});
 	});
+
+	let deleteScheduledPostButtons = document.querySelectorAll(
+		".delete-scheduled-post",
+	);
+	setupAlertModals(deleteScheduledPostButtons);
 });
--- src/views/page/scheduled_posts.php
+++ src/views/page/scheduled_posts.php
@@ -10,8 +10,8 @@ use Carbon\Carbon;
         </div>
         <?php foreach ($scheduledPosts as $post) : ?>
             <div class="divide-y divide-gray-100 dark:divide-gray-700">
-                <div class="flex w-full items-center hover:cursor-pointer scheduled-post">
-                    <div class="w-3/4 ml-4 ">
+                <div class="flex w-full items-center">
+                    <div class="w-3/4 ml-4 hover:cursor-pointer scheduled-post">
                         <div class="flex h-full flex-col justify-center">
                             <div class="mb-1.5 text-sm">
                                 <p class="scheduled-post-content text-sm mt-1 text-gray-600 dark:text-gray-400"><?= $post->getContent() ?></p>
@@ -22,7 +22,7 @@ use Carbon\Carbon;
                         </div>
                     </div>
                     <div class="w-1/4 flex h-full justify-center items-center">
-                        <button type="button" class="delete-schedule-post mt-2 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 z-10">削除</button>
+                        <button data-post-id="<?= $post->getId() ?>" data-post-type="post" type="button" class="delete-scheduled-post mt-2 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 z-10">削除</button>
                     </div>
                 </div>
             </div>
@@ -30,4 +30,8 @@ use Carbon\Carbon;
     </div>
 </div>
 
+<?php include(__DIR__ . '/../components/alert_modal.php') ?>
+<script>
+    let csrfToken = "<?= src\helpers\CrossSiteForgeryProtection::getToken() ?>"
+</script>
 <script src="/scheduledPosts.bundle.js"></script>
