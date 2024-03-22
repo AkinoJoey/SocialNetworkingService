a61f77be0156f67618c07dab3f74542c4ead4593
AkinoJoey <gakiya213@gmail.com>
2024-03-22 16:02:04 +0900

    すべてのdivにかかってしまっているクラスを修正

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -7,15 +7,13 @@
 	<script src="https://kit.fontawesome.com/51efafd327.js" crossorigin="anonymous"></script>
 </head>
 
-<!-- TODO: hover時のグレーの範囲 -->
-
 <body>
-	<div class="container mx-auto p-4">
-		<?php if (isset($user) && $user->getEmailVerified()) : ?>
+	<?php if (isset($user) && $user->getEmailVerified()) : ?>
+		<div class="container mx-auto p-4">
 			<?php include(__DIR__ . '/../components/sidebar.php') ?>
 			<?php include(__DIR__ . '/../components/bottom_nav.php') ?>
 			<?php include(__DIR__ . '/../components/new_post.php') ?>
 			<?php include(__DIR__ . '/../components/schedule.php') ?>
 			<?php include(__DIR__ . '/../components/logout_modal.php') ?>
-		<?php endif; ?>
-	</div>
+		</div>
+	<?php endif; ?>
