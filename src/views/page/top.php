925bc7537295a456e2617e67b30aceda1d2ff739
AkinoJoey <gakiya213@gmail.com>
2024-03-23 16:48:35 +0900

    マージントップを追加

--- src/views/page/top.php
+++ src/views/page/top.php
@@ -2,19 +2,19 @@
 <?php include(__DIR__ . '/../components/timeline_tab.php') ?>
 
 <!-- post -->
-<div class="container mx-auto mb-14 flex flex-col items-center justify-center p-4">
+<div class="container mx-auto mb-14 flex flex-col items-center justify-center p-4 mt-8">
 	<div id="trend_container" class="timeline-container <?php if ($tabActive !== 'trend') echo 'hidden' ?>">
 	</div>
 
 	<div id="following_container" class="timeline-container <?php if ($tabActive !== 'following') echo 'hidden' ?>">
-</div>
+	</div>
 
-<?php include(__DIR__ . '/../components/alert_modal.php') ?>
-<?php include(__DIR__ . '/../components/speed_dial.php') ?>
+	<?php include(__DIR__ . '/../components/alert_modal.php') ?>
+	<?php include(__DIR__ . '/../components/speed_dial.php') ?>
 
-<script>
-	let csrfToken = "<?= src\helpers\CrossSiteForgeryProtection::getToken() ?>"
-	let currentTab = "<?= $tabActive ?>"
-</script>
+	<script>
+		let csrfToken = "<?= src\helpers\CrossSiteForgeryProtection::getToken() ?>"
+		let currentTab = "<?= $tabActive ?>"
+	</script>
 
-<script src="/top.bundle.js"></script>
+	<script src="/top.bundle.js"></script>
