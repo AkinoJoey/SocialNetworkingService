b092c7bfeea9edbb1fd168faa1f184851994b3b0
AkinoJoey <gakiya213@gmail.com>
2024-03-23 16:47:45 +0900

    不要なクラスを削除

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -9,11 +9,9 @@
 
 <body>
 	<?php if (isset($user) && $user->getEmailVerified()) : ?>
-		<div class="container mx-auto p-4">
-			<?php include(__DIR__ . '/../components/sidebar.php') ?>
-			<?php include(__DIR__ . '/../components/bottom_nav.php') ?>
-			<?php include(__DIR__ . '/../components/new_post.php') ?>
-			<?php include(__DIR__ . '/../components/schedule.php') ?>
-			<?php include(__DIR__ . '/../components/logout_modal.php') ?>
-		</div>
+		<?php include(__DIR__ . '/../components/sidebar.php') ?>
+		<?php include(__DIR__ . '/../components/bottom_nav.php') ?>
+		<?php include(__DIR__ . '/../components/new_post.php') ?>
+		<?php include(__DIR__ . '/../components/schedule.php') ?>
+		<?php include(__DIR__ . '/../components/logout_modal.php') ?>
 	<?php endif; ?>
