b6fa1775def2298b9dd0b288ccd1e7227020796e
AkinoJoey <gakiya213@gmail.com>
2024-03-25 15:59:29 +0900

    コメントのコメント用のシーディングを作成

--- src/database/seeds_dao/CommentsDaoSeeder.php
+++ src/database/seeds_dao/CommentsDaoSeeder.php
@@ -12,19 +12,21 @@ class CommentsDaoSeeder implements Seeder
     public function seed(): void
     {
         $commentDao = DAOFactory::getCommentDAO();
-        $comments = $this->createDummyComments();
-        array_map(fn($comment)=> $commentDao->create($comment), $comments);
+        // $comments = $this->createDummyComments(); //ポストへのコメント
+        $childComments = $this->createChildComments(); // コメントへのコメント
+        // array_map(fn ($comment) => $commentDao->create($comment), $comments);
+        array_map(fn ($comment) => $commentDao->create($comment), $childComments);
     }
 
     public function createDummyComments(): array
     {
         $data = [];
         $faker = \Faker\Factory::create();
-        $numberOfDummyPosts = 300;
+        $numberOfDummyComments = 300;
         //投稿のURLとメディアのファイル名の長さ 
         $numberOfCharacters = 18;
 
-        for ($i = 0; $i < $numberOfDummyPosts; $i++) {
+        for ($i = 0; $i < $numberOfDummyComments; $i++) {
             $comment = new Comment(
                 url: bin2hex(random_bytes($numberOfCharacters / 2)),
                 userId: $faker->numberBetween(1, 20),
@@ -32,10 +34,34 @@ class CommentsDaoSeeder implements Seeder
                 postId: $faker->numberBetween(1, 100),
                 timeStamp: new DataTimeStamp($faker->dateTimeBetween('-1 years')->format('Y-m-d H:i:s'), (new DateTime())->format("Y-m-d H:i:s")),
             );
-            
+
             $data[] = $comment;
         }
 
         return $data;
+    }
+
+    public function createChildComments(): array
+    {
+        $data = [];
+        $faker = \Faker\Factory::create();
+        $numberOfDummyComments = 150;
+        //投稿のURLとメディアのファイル名の長さ 
+        $numberOfCharacters = 18;
+
+        for ($i = 0; $i < $numberOfDummyComments; $i++) {
+            $comment = new Comment(
+                url: bin2hex(random_bytes($numberOfCharacters / 2)),
+                userId: $faker->numberBetween(1, 20),
+                content: $faker->realTextBetween(10, 140, 5),
+                parentCommentId: $faker->numberBetween(1, 300),
+                timeStamp: new DataTimeStamp($faker->dateTimeBetween('-1 years')->format('Y-m-d H:i:s'), (new DateTime())->format("Y-m-d H:i:s")),
+            );
+
+            $data[] = $comment;
+        }
+
+        return $data;
+        
     }
 }
