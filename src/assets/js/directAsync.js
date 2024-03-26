63577ac1d4af173a2e64cfe50fbcb8b1aa9e8614
AkinoJoey <gakiya213@gmail.com>
2024-03-26 17:27:13 +0900

    サーバー側からstatus errorを受け取った場合、ユーザーが無効なテキストを入力した場合の振る舞いを追加

--- src/assets/js/directAsync.js
+++ src/assets/js/directAsync.js
@@ -1,73 +1,83 @@
-document.addEventListener('DOMContentLoaded', function () {
-    window.scrollTo(0, document.body.scrollHeight);
-    
-    let conn = new WebSocket('ws://localhost:8080');
-    conn.onopen = function(e) {
-        let data = {
-            type: 'join',
-            dm_thread_id: dmThreadId,
-            sender_user_id: senderUserId,
-            receiver_user_id: receiverUserId,
-        };
+document.addEventListener("DOMContentLoaded", function () {
+	window.scrollTo(0, document.body.scrollHeight);
 
-        conn.send(JSON.stringify(data));
-    };
+	let conn = new WebSocket("ws://localhost:8080");
+	conn.onopen = function (e) {
+		let data = {
+			type: "join",
+			dm_thread_id: dmThreadId,
+			sender_user_id: senderUserId,
+			receiver_user_id: receiverUserId,
+		};
 
-    const chatContainer = document.getElementById('chat_container');
-    conn.onmessage = function(e) {
-        chatContainer.innerHTML += `
-        <div class="chat chat-start">
-                <div class="avatar chat-image">
-                    <div class="w-10 rounded-full">
-                        <img alt="Tailwind CSS chat bubble component" src="${receiverUserProfileImagePath}" />
+		conn.send(JSON.stringify(data));
+	};
+
+	const chatContainer = document.getElementById("chat_container");
+	conn.onmessage = function (e) {
+		let data = JSON.parse(e.data);
+		if (data.status === "success") {
+			chatContainer.innerHTML += `
+            <div class="chat chat-start">
+                    <div class="avatar chat-image">
+                        <div class="w-10 rounded-full">
+                            <img alt="Tailwind CSS chat bubble component" src="${receiverUserProfileImagePath}" />
+                        </div>
                     </div>
-                </div>
-                <div class="chat-header">
-                    ${receiverUserAccountName}
-                    <time class="text-xs opacity-50">12:45</time>
-                </div>
-                <div class="chat-bubble text-black bg-gray-300 dark:bg-gray-700">${e.data}</div>
-                <div class="chat-footer opacity-50">Delivered</div>
-        </div>
-        `;
-        window.scrollTo(0, document.body.scrollHeight);
+                    <div class="chat-header">
+                        ${receiverUserAccountName}
+                        <time class="text-xs opacity-50">12:45</time>
+                    </div>
+                    <div class="chat-bubble text-black bg-gray-300 dark:bg-gray-700">${data.message}</div>
+                    <div class="chat-footer opacity-50">Delivered</div>
+            </div>
+            `;
+			window.scrollTo(0, document.body.scrollHeight);
+		}
+		if (data.status === "error") {
+			alert(data.message);
+		}
+	};
 
-    };
+	conn.onerror = function () {
+        alert("エラーが発生しました");
+        window.location.reload();
+	};
 
-    conn.onerror = function () {
-        alert('エラーが発生しました');
-    }
+	document.getElementById("submit_btn").addEventListener("click", function (e) {
+		e.preventDefault();
+		send();
+	});
 
+	let chatTextArea = document.getElementById("message");
+	chatTextArea.focus();
 
-    document.getElementById('submit_btn').addEventListener('click', function(e) {
-        e.preventDefault();
-        send()
-    });
-
-    let chatTextArea = document.getElementById('message');
-    chatTextArea.focus();
-
-    chatTextArea.addEventListener("keydown", function(e) {
-        if (e.key === 'Enter' && e.shiftKey) {
-            e.preventDefault();
-            send()
-        }
-    })
+	chatTextArea.addEventListener("keydown", function (e) {
+		if (e.key === "Enter" && e.shiftKey) {
+			e.preventDefault();
+			send();
+		}
+	});
 
     function send() {
-        if (chatTextArea.value === '') return;
+		if (chatTextArea.value === "") return;
 
-        let data = {
-            type: 'message',
-            dm_thread_id: dmThreadId,
-            sender_user_id: senderUserId,
-            receiver_user_id: receiverUserId,
-            message: chatTextArea.value
-        };
+		let maxLength = 140;
 
-        conn.send(JSON.stringify(data));
+		if (chatTextArea.value.length > maxLength || chatTextArea.value.trim() === "") {
+            alert("1文字以上140文字以内のテキストのみ送信可能です");
+		} else {
+			let data = {
+				type: "message",
+				dm_thread_id: dmThreadId,
+				sender_user_id: senderUserId,
+				receiver_user_id: receiverUserId,
+				message: chatTextArea.value,
+			};
 
-        chatContainer.innerHTML += `
+			conn.send(JSON.stringify(data));
+
+			chatContainer.innerHTML += `
             <div class="chat chat-end">
                 <div class="avatar chat-image">
                     <div class="w-10 rounded-full">
@@ -81,7 +91,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="chat-footer opacity-50">${new Date().toLocaleDateString("ja-JP", { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit" })}</div>
             </div>
             `;
-        chatTextArea.value = "";
-        window.scrollTo(0, document.body.scrollHeight);
-    }
-})
+			chatTextArea.value = "";
+			window.scrollTo(0, document.body.scrollHeight);
+		}
+	}
+});
