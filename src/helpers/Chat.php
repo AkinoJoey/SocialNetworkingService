6297692608ca3a0909e9e92e6cbddb78046f05b4
AkinoJoey <gakiya213@gmail.com>
2024-03-26 17:24:46 +0900

    エラーハンドリングを追加

--- src/helpers/Chat.php
+++ src/helpers/Chat.php
@@ -5,17 +5,11 @@ namespace src\helpers;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 use src\helpers\ValidationHelper;
-use src\database\data_access\DAOFactory;
-use src\models\DmMessage;
 use Exception;
 use src\helpers\ChatClient;
-use src\types\NotificationType;
-use src\models\Notification;
 use src\types\GeneralValueType;
 use src\types\PostValueType;
 
-// TODO: authenticate
-// TODO: 141文字以上ならアラートを出す
 class Chat implements MessageComponentInterface
 {
     protected $threads = []; // [threadId => [resourceId => ChatClient, resourceId=>ChatClient]]
@@ -34,7 +28,7 @@ class Chat implements MessageComponentInterface
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
-        try{
+        try {
             $data = json_decode($msg, true);
             $validatedType = ValidationHelper::chatType($data['type']);
 
@@ -44,116 +38,111 @@ class Chat implements MessageComponentInterface
                     break;
 
                 case 'message':
-                    $this->sendMessage($data);
+                    $this->sendMessage($from, $data);
                     break;
             }
         } catch (\InvalidArgumentException $e) {
-            $from->send(json_encode(['error' => $e->getMessage()]));
+            error_log($e->getMessage());
+            $from->send(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
+        } catch (\LengthException $e) {
+            error_log($e->getMessage());
+            $from->send(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
         } catch (Exception $e) {
-            $from->send(json_encode(['error' => 'エラーが発生しました']));
-            error_log('エラーが発生しました: ' . $e->getMessage());
+            error_log( $e->getMessage());
+            $from->send(json_encode(['status' => 'error', 'message' => 'エラーが発生しました']));
         }
     }
 
     public function onClose(ConnectionInterface $conn)
     {
-        // threadからclientを削除
-        $client = $this->clients[$conn->resourceId];
-        $threadId = $client->getJoinedThreadId();
-        $thread = $this->threads[$threadId];
+        if (isset($this->clients[$conn->resourceId])) {
+            // threadからclientを削除
+            $client = $this->clients[$conn->resourceId];
+            $threadId = $client->getJoinedThreadId();
+            $thread = $this->threads[$threadId];
 
-        unset($thread[$client->getConn()->resourceId]);
+            unset($thread[$client->getConn()->resourceId]);
 
-        // threadが空の場合はthreadを削除
-        if (count($thread) < 1) unset($this->threads[$threadId]);
+            // threadが空の場合はthreadを削除
+            if (count($thread) < 1) unset($this->threads[$threadId]);
 
-        // clientsから削除
-        unset($this->clients[$conn->resourceId]);
+            // clientsから削除
+            unset($this->clients[$conn->resourceId]);
 
-        echo "Connection {$conn->resourceId} has disconnected\n";
+            echo "Connection {$conn->resourceId} has disconnected\n";
+        }
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e)
     {
-        echo "An error has occurred: {$e->getMessage()}\n";
-
-        $conn->close();
+        echo "エラーが発生しました: {$e->getMessage()}\n";
     }
 
     private function join(ConnectionInterface $from, array $data): void
     {
-        // TODO: error handling
-        $required_fields = [
-            'dm_thread_id' => GeneralValueType::INT,
-            'sender_user_id' => GeneralValueType::INT,
-            'receiver_user_id' => GeneralValueType::INT,
-        ];
+        try {
+            $required_fields = [
+                'dm_thread_id' => GeneralValueType::INT,
+                'sender_user_id' => GeneralValueType::INT,
+                'receiver_user_id' => GeneralValueType::INT,
+            ];
 
-        $validatedData = ValidationHelper::validateFields($required_fields, $data, true);
+            $validatedData = ValidationHelper::validateFields($required_fields, $data);
 
-        $client = new ChatClient($from, $validatedData['sender_user_id'], $validatedData['dm_thread_id']);
+            $client = new ChatClient($from, $validatedData['sender_user_id'], $validatedData['dm_thread_id']);
 
-        $this->clients[$from->resourceId] = $client;
-        $this->threads[$validatedData['dm_thread_id']][$from->resourceId] = $client;
+            $this->clients[$from->resourceId] = $client;
+            $this->threads[$validatedData['dm_thread_id']][$from->resourceId] = $client;
 
-        echo "Starting chat! ({$from->resourceId})\n";
+            echo "Starting chat! ({$from->resourceId})\n";
+        } catch (\InvalidArgumentException $e) {
+            error_log($e->getMessage());
+            $from->send(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
+        } catch (Exception $e) {
+            error_log( $e->getMessage());
+            $from->send(json_encode(['status' => 'error', 'message' => 'エラーが発生しました']));
+        }
     }
 
-    private function sendMessage(array $data): void
+    private function sendMessage(ConnectionInterface $from, array $data): void
     {
-        $required_fields = [
-            'dm_thread_id' => GeneralValueType::INT,
-            'sender_user_id' => GeneralValueType::INT,
-            'receiver_user_id' => GeneralValueType::INT,
-            'message' => PostValueType::CONTENT //最大文字数は140文字
-        ];
+        try {
+            $required_fields = [
+                'dm_thread_id' => GeneralValueType::INT,
+                'sender_user_id' => GeneralValueType::INT,
+                'receiver_user_id' => GeneralValueType::INT,
+                'message' => PostValueType::CONTENT //最大文字数は140文字
+            ];
 
-        $validatedData = ValidationHelper::validateFields($required_fields, $data, true);
+            $validatedData = ValidationHelper::validateFields($required_fields, $data);
 
-        $encryptedData = CipherHelper::encryptMessage($validatedData['message']);
+            ChatHelper::createMessage($validatedData['message'], $validatedData['sender_user_id'], $validatedData['receiver_user_id'], $validatedData['dm_thread_id']);
 
-        $messageDao = DAOFactory::getDmMessageDAO();
-        $dmMessage = new DmMessage(
-            message: $encryptedData['encrypted'],
-            iv: $encryptedData['iv'],
-            senderUserId: $validatedData['sender_user_id'],
-            receiverUserId: $validatedData['receiver_user_id'],
-            dmThreadId: $validatedData['dm_thread_id'],
-        );
+            $thread = $this->threads[$validatedData['dm_thread_id']] ?? null;
+            //　スレッドが作成されている場合、receiver_user_idにだけメッセージを送る
+            if (isset($thread)) {
+                foreach (array_values($thread) as $client) {
+                    if ($client->getUserId() === $validatedData['receiver_user_id']) {
 
-        $success = $messageDao->create($dmMessage);
-        // TODO: try-catch
-        if (!$success) throw new Exception('Failed to create a message!');
+                        $data = json_encode(['status' => 'success', 'message' => $validatedData['message']]);
+                        $client->getConn()->send($data);
+                    } else {
 
-        $thread = $this->threads[$validatedData['dm_thread_id']] ?? null;
-        //　スレッドが作成されている場合、receiver_user_idにだけメッセージを送る
-        if (isset($thread)) {
-            foreach (array_values($thread) as $client) {
-                if ($client->getUserId() === $validatedData['receiver_user_id']) {
-                    $client->getConn()->send($validatedData['message']);
-                } else {
-                    // リアルタイム通信しない場合、DMの未読通知がない場合は通知を作成する。ある場合は通知のupdatedAtを更新する
-                    $notificationDao = DAOFactory::getNotificationDAO();
-                    $notificationId =  $notificationDao->getUnreadDMNotificationId($validatedData['receiver_user_id'], $validatedData['sender_user_id']);
-
-                    if(isset($notificationId)){
-                        $success = $notificationDao->updateUpdatedAt($notificationId);
-                        if (!$success) throw new Exception('Failed to update a notification!');
-                    }else{
-                        
-                        $notification = new Notification(
-                            id: $notificationId,
-                            userId: $validatedData['receiver_user_id'],
-                            sourceId: $validatedData['sender_user_id'],
-                            notificationType: NotificationType::DM->value,
-                            dmThreadId: $validatedData['dm_thread_id'],
-                        );
-
-                        $success = $notificationDao->create($notification);
-                        if (!$success) throw new Exception('Failed to create a notification!');
+                        // リアルタイム通信しない場合、DMの未読通知がない場合はNotificationを作成する。
+                        // 作成済みのNotificationが存在する場合はupdatedAtを更新する
+                        ChatHelper::createOrUpdateNotification($validatedData['receiver_user_id'], $validatedData['sender_user_id'], $validatedData['dm_thread_id']);
                     }
                 }
             }
+        } catch (\InvalidArgumentException $e) {
+            error_log($e->getMessage());
+            $from->send(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
+        } catch (\LengthException $e) {
+            error_log($e->getMessage());
+            $from->send(json_encode(['status' => 'error', 'message' => $e->getMessage()]));
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            $from->send(json_encode(['status' => 'error', 'message' => 'エラーが発生しました']));
         }
     }
 }
