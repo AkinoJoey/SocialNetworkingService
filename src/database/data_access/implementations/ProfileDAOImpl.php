9bcf8948b29ea67f1f26265b87ecf44a710e9f66
AkinoJoey <gakiya213@gmail.com>
2024-03-27 19:08:11 +0900

    create関数を修正

--- src/database/data_access/implementations/ProfileDAOImpl.php
+++ src/database/data_access/implementations/ProfileDAOImpl.php
@@ -23,7 +23,7 @@ class ProfileDAOImpl implements ProfileDAO
             [
                 $profile->getAge(),
                 $profile->getLocation(),
-                preg_replace("/(\R{3,})/", "\n\n", $profile->getDescription()),//3行以上の改行は2行にする
+                $profile->getDescription() !== null? preg_replace("/(\R{3,})/", "\n\n", $profile->getDescription()): null,//3行以上の改行は2行にする
                 $profile->getProfileImagePath(),
                 $profile->getExtension(),
                 $profile->getUserId()
