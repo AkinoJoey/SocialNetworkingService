a7d4780a5055b8124056ba5b8a5738af4bea28ba
AkinoJoey <gakiya213@gmail.com>
2024-03-28 18:19:00 +0900

    faviconを追加

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -5,6 +5,8 @@
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<script src="https://kit.fontawesome.com/51efafd327.js" crossorigin="anonymous"></script>
+	<link rel="icon" href="/images/favicon.ico" sizes="any">
+	<title>ten</title>
 </head>
 
 <body>
