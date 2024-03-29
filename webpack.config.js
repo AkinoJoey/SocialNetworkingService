c8e51a66f2ea32262ee7845132c9681983bbf43a
AkinoJoey <gakiya213@gmail.com>
2024-03-29 14:01:00 +0900

    webpack.DefinePluginを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
+const webpack = require("webpack");
+
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
+	mode: isDev ? "development" : "production",
 	entry: {
 		main: "./src/assets/js/index.js",
 		posts: "./src/assets/js/posts.js",
@@ -14,15 +18,15 @@ module.exports = {
 		scheduledPosts: "./src/assets/js/scheduledPosts.js",
 		guest: "./src/assets/js/guest.js",
 		verifyForgotPassword: "./src/assets/js/verifyForgotPassword.js",
-		signup: './src/assets/js/signup.js',
-		login: './src/assets/js/login.js',
-		forgotPassword: "./src/assets/js/forgotPassword.js"
+		signup: "./src/assets/js/signup.js",
+		login: "./src/assets/js/login.js",
+		forgotPassword: "./src/assets/js/forgotPassword.js",
 	},
 	output: {
 		path: path.resolve(__dirname, "public"),
 		filename: "[name].bundle.js",
 	},
-	
+
 	module: {
 		rules: [
 			{
@@ -31,4 +35,9 @@ module.exports = {
 			},
 		],
 	},
+	plugins: [
+		new webpack.DefinePlugin({
+			DEVELOPMENT: JSON.stringify(isDev),
+		}),
+	],
 };
