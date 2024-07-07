7b0ae5dc0cf205d0c79d2afaa00cb8c5331c4f14
AkinoJoey <gakiya213@gmail.com>
2024-07-07 18:01:29 +0900

    タイムライン表示の関数を修正

--- src/database/data_access/implementations/PostDAOImpl.php
+++ src/database/data_access/implementations/PostDAOImpl.php
@@ -261,10 +261,10 @@ class PostDAOImpl implements PostDAO
                 GROUP BY c.post_id
             ),
             like_counts AS(
-                SELECT p.id, COUNT(*) AS number_of_likes
-                FROM posts p
-                WHERE p.id IN (SELECT id FROM post_data)
-                GROUP BY p.id
+                SELECT pl.post_id, COUNT(*) AS number_of_likes
+                FROM post_likes pl
+                WHERE pl.post_id IN (SELECT id FROM post_data)
+                GROUP BY pl.post_id
             ),
             user_data AS(
                 SELECT u.id, u.account_name, u.username
@@ -288,7 +288,7 @@ class PostDAOImpl implements PostDAO
             COALESCE(ul.is_like, 0)AS is_like
             FROM post_data t
             LEFT JOIN comment_counts cc ON t.id = cc.post_id
-            LEFT JOIN like_counts lc ON t.id = lc.id
+            LEFT JOIN like_counts lc ON t.id = lc.post_id
             LEFT JOIN user_data ud ON t.user_id = ud.id
             LEFT JOIN user_likes ul ON t.id = ul.post_id
             LEFT JOIN profile_data pd ON t.user_id = pd.user_id;
@@ -323,50 +323,53 @@ class PostDAOImpl implements PostDAO
         $mysqli = DatabaseManager::getMysqliConnection();
         $query =
             <<<SQL
-            WITH top_20 AS (
-                SELECT p.id, p.content, p.url, p.media_path, p.extension, p.status, p.created_at, p.updated_at, p.user_id,
-                    COUNT(pl.post_id) AS number_of_likes
-                FROM posts p
-                INNER JOIN post_likes pl ON p.id = pl.post_id 
-                WHERE p.status = 'public'
-                GROUP BY p.id
+            with top_20_likes AS(
+                SELECT pl.post_id, COUNT(*) AS number_of_likes
+                FROM post_likes pl 
+                GROUP BY pl.post_id
                 ORDER BY number_of_likes DESC
                 LIMIT ?, ?
+            ),post_data AS(
+                SELECT p.id, p.content,p.url,p.media_path,p.extension,p.scheduled_at,p.status,p.user_id,p.created_at,p.updated_at, COALESCE(number_of_likes, 0) AS number_of_likes
+                FROM posts p 
+                LEFT JOIN top_20_likes top ON p.id = top.post_id 
+                WHERE status = 'public' AND DATE(p.created_at) = CURDATE() 
+                ORDER BY number_of_likes DESC, p.created_at DESC
+                LIMIT ?
             ),
-            comment_counts AS (
+            comment_data AS(
                 SELECT c.post_id, COUNT(*) AS number_of_comments
                 FROM comments c
-                WHERE c.post_id IN (SELECT id FROM top_20)
+                WHERE c.post_id IN (SELECT post_data.id FROM post_data)
                 GROUP BY c.post_id
             ),
             user_data AS(
                 SELECT u.id, u.account_name, u.username
-                FROM users u
-                WHERE u.id IN (SELECT top_20.user_id FROM top_20)
+                FROM users u 
+                WHERE u.id IN (SELECT post_data.user_id FROM post_data)
             ),
             profile_data AS(
-                SELECT p.user_id, p.profile_image_path, p.extension AS profile_image_extension
-                FROM profiles p 
-                WHERE p.user_id IN (SELECT top_20.user_id FROM top_20)
+                SELECT *
+                FROM profiles pr
+                WHERE pr.user_id IN (SELECT post_data.user_id FROM post_data)
             ),
             user_likes AS (
                 SELECT pl.post_id, pl.user_id, 1 AS is_like
                 FROM post_likes pl
-                WHERE pl.user_id = ? AND pl.post_id IN (SELECT id FROM top_20)
+                WHERE pl.user_id = ? AND pl.post_id IN (SELECT post_data.id FROM post_data)
             )
-            SELECT t.*, ud.username,ud.account_name , pd.profile_image_path, pd.profile_image_extension,
-            COALESCE(cc.number_of_comments, 0) AS number_of_comments,
-            COALESCE(ul.is_like, 0) AS is_like
-            FROM top_20 t
-            LEFT JOIN comment_counts cc ON t.id = cc.post_id
-            LEFT JOIN user_data ud ON t.user_id = ud.id
-            LEFT JOIN profile_data pd ON t.user_id = pd.user_id
-            LEFT JOIN user_likes ul ON t.id = ul.post_id
-            ORDER BY t.created_at DESC,
-            number_of_likes DESC;
+            SELECT pd.*, COALESCE(cd.number_of_comments,0) AS number_of_comments ,ud.account_name, ud.username, pr.profile_image_path, pr.extension AS profile_image_extension,
+                    COALESCE(ul.is_like, 0) AS is_like
+            FROM post_data pd
+            LEFT JOIN comment_data cd ON pd.id = cd.post_id
+            LEFT JOIN user_data ud ON pd.user_id = ud.id
+            LEFT JOIN profile_data pr ON pd.user_id = pr.user_id
+            LEFT JOIN user_likes ul ON pd.id = ul.post_id
+            ORDER BY pd.number_of_likes DESC,
+            pd.created_at DESC;
             SQL;
 
-        $results = $mysqli->prepareAndFetchAll($query, "iii", [$offset, $limit,$userId]);
+        $results = $mysqli->prepareAndFetchAll($query, "iiii", [$offset, $limit, $limit,$userId]);
 
         return $results === null ? [] : $this->rawDataToPosts($results);
     }
@@ -376,36 +379,45 @@ class PostDAOImpl implements PostDAO
         $mysqli = DatabaseManager::getMysqliConnection();
         $query =
             <<<SQL
-            WITH top_20 AS (
-                SELECT p.id, p.content, p.url, p.media_path, p.extension, p.status, p.created_at, p.updated_at, p.user_id,
-                    COUNT(pl.post_id) AS number_of_likes
-                FROM posts p
-                INNER JOIN post_likes pl ON p.id = pl.post_id 
-                WHERE p.status = 'public'
-                GROUP BY p.id
+            with top_20_likes AS(
+                SELECT pl.post_id, COUNT(*) AS number_of_likes
+                FROM post_likes pl 
+                GROUP BY pl.post_id
                 ORDER BY number_of_likes DESC
                 LIMIT ?, ?
+            ),post_data AS(
+                SELECT p.id, p.content,p.url,p.media_path,p.extension,p.scheduled_at,p.status,p.user_id,p.created_at,p.updated_at,
+                COALESCE(number_of_likes, 0) AS number_of_likes
+                FROM posts p 
+                LEFT JOIN top_20_likes top ON p.id = top.post_id 
+                WHERE status = 'public' AND DATE(p.created_at) = CURDATE() 
+                ORDER BY number_of_likes DESC, p.created_at DESC
+                LIMIT ?
             ),
-            comment_counts AS (
+            comment_data AS(
                 SELECT c.post_id, COUNT(*) AS number_of_comments
                 FROM comments c
-                WHERE c.post_id IN (SELECT id FROM top_20)
+                WHERE c.post_id IN (SELECT post_data.id FROM post_data)
                 GROUP BY c.post_id
             ),
             user_data AS(
                 SELECT u.id, u.account_name, u.username
-                FROM users u
-                WHERE u.id IN (SELECT top_20.user_id FROM top_20)
+                FROM users u 
+                WHERE u.id IN (SELECT post_data.user_id FROM post_data)
+            ),
+            profile_data AS(
+                SELECT *
+                FROM profiles pr
+                WHERE pr.user_id IN (SELECT post_data.user_id FROM post_data)
             )
-            SELECT t.*, ud.username,ud.account_name ,COALESCE(cc.number_of_comments, 0) AS number_of_comments
-            FROM top_20 t
-            LEFT JOIN comment_counts cc ON t.id = cc.post_id
-            LEFT JOIN user_data ud ON t.user_id = ud.id
-            ORDER BY t.created_at DESC,
-            number_of_likes DESC;
+            SELECT pd.*, COALESCE(cd.number_of_comments,0) AS number_of_comments, ud.account_name, ud.username, pr.profile_image_path, pr.extension AS profile_image_extension
+            FROM post_data pd
+            LEFT JOIN comment_data cd ON pd.id = cd.post_id
+            LEFT JOIN user_data ud ON pd.user_id = ud.id
+            LEFT JOIN profile_data pr ON pd.user_id = pr.user_id;
             SQL;
 
-        $results = $mysqli->prepareAndFetchAll($query, "ii", [$offset, $limit]);
+        $results = $mysqli->prepareAndFetchAll($query, "iii", [$offset, $limit, $limit]);
 
         return $results === null ? [] : $this->rawDataToPosts($results);
     }
@@ -435,42 +447,51 @@ class PostDAOImpl implements PostDAO
         $mysqli = DatabaseManager::getMysqliConnection();
 
         $query = <<<SQL
-            WITH post_data AS (
-                SELECT p.*,
-                    u.account_name, u.username
-                FROM posts p
-                INNER JOIN users u ON p.user_id = u.id
-                WHERE u.username = ? AND p.status = 'public'
+            WITH user_data AS (
+                SELECT u.id, u.account_name, u.username
+                FROM users u 
+                WHERE u.username = ?
             ),
-            comment_data AS (
-                SELECT pc.post_id, COUNT(*) AS number_of_comments
-                FROM comments pc
-                GROUP BY pc.post_id
+            post_data AS(
+                    SELECT p.*
+                    FROM posts p 
+                    WHERE p.user_id IN (SELECT id FROM user_data) AND p.status = 'public'
+                    ORDER BY p.created_at DESC
+                    LIMIT ?, ?
             ),
-            like_data AS (
+            comment_counts AS (
+                SELECT c.post_id, COUNT(*) AS number_of_comments
+                FROM comments c
+                WHERE c.post_id IN (SELECT id FROM post_data)
+                GROUP BY c.post_id
+            ),
+            like_counts AS(
                 SELECT pl.post_id, COUNT(*) AS number_of_likes
                 FROM post_likes pl
+                WHERE pl.post_id IN (SELECT id FROM post_data)
                 GROUP BY pl.post_id
             ),
+            profile_data AS(
+                    SELECT pr.user_id, pr.profile_image_path, pr.extension AS profile_image_extension
+                    FROM profiles pr
+                    WHERE pr.user_id IN (SELECT post_data.user_id FROM post_data)
+            ),
             user_likes AS (
-                SELECT pl.post_id, COUNT(*) AS is_like
+                SELECT pl.post_id, pl.user_id, 1 AS is_like
                 FROM post_likes pl
-                WHERE pl.user_id = ?
-                GROUP BY pl.post_id
+                WHERE pl.user_id = ? AND pl.post_id IN (SELECT id FROM post_data)
             )
-            SELECT pd.id, pd.content, pd.url, pd.media_path, pd.extension, pd.status,  pd.created_at, pd.updated_at ,pd.user_id, pr.profile_image_path, pr.extension AS profile_image_extension ,pd.account_name, pd.username,
-                COALESCE(cd.number_of_comments, 0) AS number_of_comments,
-                COALESCE(ld.number_of_likes, 0) AS number_of_likes,
-                COALESCE(ul.is_like, 0) AS is_like
+            SELECT pd.*, ud.account_name, ud.username, COALESCE(cc.number_of_comments, 0) AS number_of_comments, COALESCE(lc.number_of_likes,0) AS number_of_likes,
+            prd.profile_image_path, prd.profile_image_extension, COALESCE(ul.is_like,0) AS is_like
             FROM post_data pd
-            LEFT JOIN comment_data cd ON pd.id = cd.post_id
-            LEFT JOIN like_data ld ON pd.id = ld.post_id
-            LEFT JOIN user_likes ul ON pd.id = ul.post_id
-            LEFT JOIN profiles pr on pd.user_id = pr.user_id
-            ORDER BY pd.created_at DESC LIMIT ?, ?;
+            LEFT JOIN user_data ud ON pd.user_id = ud.id
+            LEFT JOIN comment_counts cc ON pd.id = cc.post_id
+            LEFT JOIN like_counts lc ON pd.id = lc.post_id
+            LEFT JOIN profile_data prd ON pd.user_id = prd.user_id
+            LEFT JOIN user_likes ul ON pd.id = ul.post_id;
             SQL;
 
-        $results = $mysqli->prepareAndFetchAll($query, "siii", [$username, $userId, $offset, $limit]);
+        $results = $mysqli->prepareAndFetchAll($query, "siii", [$username,$offset, $limit ,$userId]);
 
         return $results === null ? [] : $this->rawDataToPosts($results);
     }
